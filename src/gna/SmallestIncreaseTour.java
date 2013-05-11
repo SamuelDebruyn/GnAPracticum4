281d75cc69f4e80b2a8a0fbd4897aaab63b721fe
SamuelDebruyn <github.s@muel.be>
2013-05-11 16:28:23 +0200

    smallest increase enkel nog het vergelijken

--- src/gna/SmallestIncreaseTour.java
+++ src/gna/SmallestIncreaseTour.java
@@ -11,6 +11,43 @@ public class SmallestIncreaseTour extends IncrementallyConstructedTour {
 
 	@Override
 	public void insert(Point point) {
-		throw new RuntimeException("not implemented");
+		
+		if(this.getVisitSequence().size() <= 1){
+			this.addToTour(point);
+			return;
+		}
+		
+		double currentTotalDistance = this.getTotalDistance();
+		
+		int bestIndex = 0;
+		double bestTotalDistance = this.newPossibleTotalDistance(0, point, currentTotalDistance);
+		
+		for(int i = 0; i < this.getVisitSequence().size(); i++){
+			double currentPossibleTotalDistance = this.newPossibleTotalDistance(i, point, currentTotalDistance);
+			if(currentPossibleTotalDistance < bestTotalDistance){
+				bestIndex = i;
+				bestTotalDistance = currentPossibleTotalDistance;
+			}
+		}
+		
+		this.addToTour(bestIndex + 1, point);
+		
+	}
+	
+	private double newPossibleTotalDistance(int index, Point point, double cachedTotalDistance){
+		//TODO
+		return 0;
+	}
+	
+	private int previousIndex(int index){
+		if(index == 0)
+			return this.getVisitSequence().size() - 1;
+		return index - 1;
+	}
+	
+	private int nextIndex(int index){
+		if(index == this.getVisitSequence().size() - 1)
+			return 0;
+		return index + 1;
 	}
 }
