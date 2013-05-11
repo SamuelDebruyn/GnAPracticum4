901cd2961a44fbcba58b64d18c46a6658d7c132b
SamuelDebruyn <github.s@muel.be>
2013-05-11 15:31:47 +0200

    getTotalDistance en getVisitSequence (onvolledig)

--- src/gna/IncrementallyConstructedTour.java
+++ src/gna/IncrementallyConstructedTour.java
@@ -1,20 +1,64 @@
 package gna;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Tours that are constructed incrementally by inserting the elements in the
  * given world one by one.
  */
 public abstract class IncrementallyConstructedTour extends Tour {
+	
+	private final ArrayList<Point> currentTour = new ArrayList<Point>();
 
-  public IncrementallyConstructedTour(World world) {
-    super(world);
-    /**
-     * Note: we expect the elements to be inserted in the same order they are added to the world.
-     */
-    for (Point point : world.getPoints()) {
-      insert(point);
-    }
-  }
+	public IncrementallyConstructedTour(World world) {
+		super(world);
+		/**
+		 * Note: we expect the elements to be inserted in the same order they
+		 * are added to the world.
+		 */
+		for (Point point : world.getPoints()) {
+			insert(point);
+		}
+	}
 
-  protected abstract void insert(Point point);
+	protected abstract void insert(Point point);
+	
+	protected void addToTour(Point p){
+		this.getVisitSequence().add(p);				
+	}
+	
+	@Override
+	public List<Point> getVisitSequence() {
+		//TODO: vragen of dit toegelaten is
+		return currentTour;
+	}
+	
+	@Override
+	public double getTotalDistance() {
+		//TODO: vragen of dit toegelaten is
+		//TODO: vragen of de afstand van laatste naar eerste ook moet bijgeteld worden (ook bij 2 punten?)
+		
+		double totalDistance = 0;
+		
+		if(this.getVisitSequence().isEmpty())
+			return totalDistance;
+			
+		Iterator<Point> itr = this.getVisitSequence().iterator();
+		
+		Point previous = itr.next();
+		
+		while(itr.hasNext()){
+			
+			Point current = itr.next();
+			totalDistance += previous.distanceTo(current);
+			previous = current;
+			
+		}
+		
+		return totalDistance;
+
+	}
+
 }
--- src/gna/SmallestIncreaseTour.java
+++ src/gna/SmallestIncreaseTour.java
@@ -1,28 +1,16 @@
 package gna;
 
-import java.util.List;
-
 /**
  * A tour constructed using the smallest increase heuristic.
  */
 public class SmallestIncreaseTour extends IncrementallyConstructedTour {
-  
-  public SmallestIncreaseTour(World world) {
-    super(world);
-  }
 
-  @Override
-  public void insert(Point point) {
-    throw new RuntimeException("not implemented");
-  }
+	public SmallestIncreaseTour(World world) {
+		super(world);
+	}
 
-  @Override
-  public double getTotalDistance() {
-    throw new RuntimeException("not implemented");
-  }
-
-  @Override
-  public List<Point> getVisitSequence() {
-    throw new RuntimeException("not implemented");
-  }
+	@Override
+	public void insert(Point point) {
+		throw new RuntimeException("not implemented");
+	}
 }
